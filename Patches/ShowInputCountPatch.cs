a1412eff58dfc5ed92ec653650550e67b144ae58
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-12 17:36:18 -0500

    Update for Volc 1.31.

--- Patches/ShowInputCountPatch.cs
+++ Patches/ShowInputCountPatch.cs
@@ -3,35 +3,35 @@ using System.Text;
 using HarmonyLib;
 using JetBrains.Annotations;
 
-namespace Recipe_Available_Input_Count.Patches {
-    [HarmonyPatch]
+namespace Recipe_Available_Input_Count.Patches;
+
+[HarmonyPatch]
+[UsedImplicitly]
+public static class ShowInputCountPatch {
+    [HarmonyTargetMethod]
     [UsedImplicitly]
-    public static class ShowInputCountPatch {
-        [HarmonyTargetMethod]
-        [UsedImplicitly]
-        public static MethodBase TargetMethod() {
-            return typeof(FactoryUi).GetMethod(nameof(FactoryUi.WriteRecipeInputs), BindingFlags.NonPublic | BindingFlags.Instance);
+    public static MethodBase TargetMethod() {
+        return typeof(FactoryUi).GetMethod(nameof(FactoryUi.WriteRecipeInputs), BindingFlags.NonPublic | BindingFlags.Instance);
+    }
+
+    [UsedImplicitly]
+    [HarmonyPrefix]
+    public static bool Prefix(ref FactoryUi __instance, ref RichTextWriter result) {
+        if (FactoryUi.m_availabilityInfo.Inputs.Count > 0) result.Text.AppendLine();
+
+        foreach (var inputInfo in FactoryUi.m_availabilityInfo.Inputs) {
+            var inputAmount = inputInfo.AmountRequired;
+            var haveAmount  = __instance.m_producer.GetPullInventory().GetAmount(inputInfo.Item, int.MaxValue);
+
+            // Show input amount needed.
+            result.CurrentStyle = inputAmount > haveAmount ? "TextError" : "Text";
+            result.Text.ConcatFormat(__instance.Texts.InputFormat.Text, inputInfo.AmountRequired, inputInfo.Item.Name);
+            // Show available amount.
+            result.CurrentStyle = "Text";
+            result.Text.ConcatFormat(__instance.Texts.InputAvailableFormat.Text, haveAmount);
+
+            result.Text.AppendLine();
         }
-
-        [UsedImplicitly]
-        [HarmonyPrefix]
-        public static bool Prefix(ref FactoryUi __instance, ref RichTextWriter result) {
-            if (FactoryUi.m_availabilityInfo.Inputs.Count > 0) result.Text.AppendLine();
-
-            foreach (var inputInfo in FactoryUi.m_availabilityInfo.Inputs) {
-                var inputAmount = inputInfo.AmountRequired;
-                var haveAmount  = __instance.m_producer.GetPullInventory().GetAmount(inputInfo.Item, int.MaxValue);
-
-                // Show input amount needed.
-                result.CurrentStyle = inputAmount > haveAmount ? "TextError" : "Text";
-                result.Text.ConcatFormat(__instance.Texts.InputFormat.Text, inputInfo.AmountRequired, inputInfo.Item.Name);
-                // Show available amount.
-                result.CurrentStyle = "Text";
-                result.Text.ConcatFormat(__instance.Texts.InputAvailableFormat.Text, haveAmount);
-
-                result.Text.AppendLine();
-            }
-            return false;
-        }
+        return false;
     }
 }
